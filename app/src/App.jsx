f52aaeefd015676bb61e3c04fe3df4481a30e8c3
BiveKKarki <karki777bivek@gmail.com>
2023-09-03 21:42:57 +0545

    add global styled component and add css to these

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,21 +4,45 @@ function App() {
 
   return (
     <>
-      <MainContainer>
-        <TopContauner>
-          <div className='logo'><img src="/logo.svg" alt='logo' /></div>
-        </TopContauner>
-      </MainContainer>
+      <Container>
+        <TopContainer>
+          <div className='logo'>
+            <img src="/logo.svg" alt='logo' />
+          </div>
+
+          <div className='search'>
+            <input placeholder='Search Food...' />
+          </div>
+        </TopContainer>
+      </Container>
     </>
   )
 }
 
 export default App
 
-const MainContainer = styled.div`
+const Container = styled.div`
+  width: 1200px;
+  margin: 0 auto;
 
 `;
 
 const TopContainer = styled.div`
+  min-height: 140px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 16px;
 
+  .search{
+    input{
+      background-color: transparent;
+      border: 1px solid red;
+      color: #fff;
+      border-radius: 5px;
+      height: 30px;
+      font-size: 16px;
+      padding: 0 10px;
+    }
+  }
 `;
